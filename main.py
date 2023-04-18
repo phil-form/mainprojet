bcd9296df3fa9c4abc088ed259b48620b574a000
philippe.stons <philippe.stons@bstorm.be>
2023-04-18 16:23:58 +0200

    update main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from librray.main import print_hi
+
 # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
+print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
